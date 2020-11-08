175317bed2050373cb2640cf9d7ff03fcf0f2f0a
rebec <ralegria77@gmail.com>
2020-11-07 17:03:07 -0800

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,12 +15,14 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).reject {|i| i.empty?}.length
+    self.split(/[.!?]\s/).count 
   end
 
 end
 
 # count_sentences - all below work
-# self.split(/[.?!]/).reject(&:empty?).count
-# self.split(' ').count { |word| word.sentence? || word.question? || word.exclamation? }
+# self.split(/[.!?]/).reject {|i| i.empty?}.length
+# self.split(/[.?!]/).reject(&:empty?).count - refactor of submitted method
+# self.split(' ').count { |word| word.sentence? || word.question? || word.exclamation? } - helper methods
 # self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+# self.split(/[[!?.]]\s/).count
